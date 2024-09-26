34a0325bb2cac6658204369aca534f88ebc9ace2
ultron-001 <142603597+ultron-001@users.noreply.github.com>
2024-09-26 12:59:32 +0100

    icon change

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,6 @@ import {Link} from "react-router-dom";
 export default function HomePage(){
     return(
         <div>
-            <Link to={"/signup/"}>Signin</Link>
             <BlogListView />
         </div>
     )
